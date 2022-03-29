89c13569359d15b9f2761395bde600ff641913ca
4Karolis <4karolis.t@gmail.com>
2022-03-29 21:24:44 +0300

    Update Program.cs

--- Bilietavimo_Sistema/Program.cs
+++ Bilietavimo_Sistema/Program.cs
@@ -12,72 +12,88 @@ namespace Bilietavimo_Sistema
             Console.WriteLine("[1] PARDUOTI bilietus, [2] KURTI bilietus");
             int pirmasPasirinkimas = int.Parse(Console.ReadLine());
 
-            Antras(pirmasPasirinkimas);
-            Trecias(antrasPasirinkimas);
-            
-        }
-        public static void Trecias(int antrasPasirinkimas)
-        {
-             antrasPasirinkimas = BilietoTipas();
-
-            switch (antrasPasirinkimas)
-            {
-                case 1:
-                    BilietuKiekis();
-                    break;
-                case 2:
-                    BilietuKiekis();
-                    break;
-                case 3:
-                    BilietuKiekis();
-                    break;
-            }
-            //BilietuKiekis();
-        }
-        public static void Antras(int pirmasPasirinkimas)
-        {
-            //pirmasPasirinkimas = Meniu();
             switch (pirmasPasirinkimas)
             {
                 case 1:
-                    BilietoTipas();                    
+                Console.WriteLine("Pasirinkite bilieto tipa: [1] - 10 EUR, [2] - 20 EUR, [3] - 3 EUR");
+                int antrasPasirinkimas = int.Parse(Console.ReadLine());
                     break;
-                case 2:
-                    BilietoTipas();
+                switch (antrasPasirinkimas)
+                {
+                case 1:
+                Console.WriteLine("Iveskite kieki:");
+                int kiekis = int.Parse(Console.ReadLine());
                     break;
-                case 3:
-                    BilietoTipas();
-                    break;
+                }
             }
-             
-        }
-        public static void KurtiBilietus10()
-        {
+            
+
+            //Antras(pirmasPasirinkimas);
+            //Trecias(antrasPasirinkimas);
 
         }
-        public static void SarasoKurimas()
-        {
-            List<int> SukurtiBilietai10 = new List<int>();
-            List<int> SukurtiBilietai20 = new List<int>();
-            List<int> SukurtiBilietai30 = new List<int>();
-        }
-        public static int BilietuKiekis()
-        {
-            Console.WriteLine("Iveskite kieki: ");
-            int kiekis = int.Parse(Console.ReadLine());
-            return kiekis;
-        }
-        public static int BilietoTipas()
-        {
-            Console.WriteLine("Pasirinkite bilieto tipa: [1] - 10 EUR, [2] - 20 EUR, [3] - 3 EUR");
-            int antrasPasirinkimas = int.Parse(Console.ReadLine());
-            return antrasPasirinkimas;
-        }
-        public static int Meniu()
-        {
-            Console.WriteLine("[1] PARDUOTI bilietus, [2] KURTI bilietus");
-            int pirmasPasirinkimas = int.Parse(Console.ReadLine());
-            return pirmasPasirinkimas;
-        }
+        //public static void Trecias(int antrasPasirinkimas)
+        //{
+        //     antrasPasirinkimas = BilietoTipas();
+
+        //    switch (antrasPasirinkimas)
+        //    {
+        //        case 1:
+        //            BilietuKiekis();
+        //            break;
+        //        case 2:
+        //            BilietuKiekis();
+        //            break;
+        //        case 3:
+        //            BilietuKiekis();
+        //            break;
+        //    }
+        //    //BilietuKiekis();
+        //}
+        //public static void Antras(int pirmasPasirinkimas)
+        //{
+        //    //pirmasPasirinkimas = Meniu();
+        //    switch (pirmasPasirinkimas)
+        //    {
+        //        case 1:
+        //            BilietoTipas();                    
+        //            break;
+        //        case 2:
+        //            BilietoTipas();
+        //            break;
+        //        case 3:
+        //            BilietoTipas();
+        //            break;
+        //    }
+             
+        //}
+        //public static void KurtiBilietus10()
+        //{
+
+        //}
+        //public static void SarasoKurimas()
+        //{
+        //    List<int> SukurtiBilietai10 = new List<int>();
+        //    List<int> SukurtiBilietai20 = new List<int>();
+        //    List<int> SukurtiBilietai30 = new List<int>();
+        //}
+        //public static int BilietuKiekis()
+        //{
+        //    Console.WriteLine("Iveskite kieki: ");
+        //    int kiekis = int.Parse(Console.ReadLine());
+        //    return kiekis;
+        //}
+        //public static int BilietoTipas()
+        //{
+        //    Console.WriteLine("Pasirinkite bilieto tipa: [1] - 10 EUR, [2] - 20 EUR, [3] - 3 EUR");
+        //    int antrasPasirinkimas = int.Parse(Console.ReadLine());
+        //    return antrasPasirinkimas;
+        //}
+        //public static int Meniu()
+        //{
+        //    Console.WriteLine("[1] PARDUOTI bilietus, [2] KURTI bilietus");
+        //    int pirmasPasirinkimas = int.Parse(Console.ReadLine());
+        //    return pirmasPasirinkimas;
+        //}
     }
 }
